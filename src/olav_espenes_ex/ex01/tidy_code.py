9e2f9d23cfa4d02b3505d1fdeded558e2d848d1f
Olav Vikøren Espenes <olaves@nmbu.no>
2019-09-23 09:38:12 +0200

    Revert "Fixed tidy_code according to PEP8."
    This reverts commit 57bdb7ff4245471ca1452702d8c92bf783b81d5f.

--- src/olav_espenes_ex/ex01/tidy_code.py
+++ src/olav_espenes_ex/ex01/tidy_code.py
@@ -2,26 +2,18 @@ __author__ = 'Olav Espenes'
 __email__ = 'olaves@nmbu.no'
 
 from random import randint
-
-
 def your_guess():
     input_guess = 0
     while input_guess < 1:
         input_guess = int(input('Your guess: '))
     return input_guess
 
-
 def two_dices_throw():
     return randint(1, 6) + randint(1, 6)
-
-
 """Throwes two dices and add those together"""
-
 
 def guess_a_throw(throw, guess):
     return throw == guess
-
-
 """Check if your throw are equal to your guess"""
 
 if __name__ == '__main__':
