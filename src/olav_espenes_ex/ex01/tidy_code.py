f09c82012fa068976581867e13e4617e00a2f1c9
Olav Vikøren Espenes <olaves@nmbu.no>
2019-09-23 09:47:05 +0200

    Fixed task D again.

--- src/olav_espenes_ex/ex01/tidy_code.py
+++ src/olav_espenes_ex/ex01/tidy_code.py
@@ -1,6 +1,7 @@
 __author__ = 'Olav Espenes'
 __email__ = 'olaves@nmbu.no'
 
+
 from random import randint
 def your_guess():
     input_guess = 0
@@ -8,12 +9,18 @@ def your_guess():
         input_guess = int(input('Your guess: '))
     return input_guess
 
+
 def two_dices_throw():
     return randint(1, 6) + randint(1, 6)
+
+
 """Throwes two dices and add those together"""
+
 
 def guess_a_throw(throw, guess):
     return throw == guess
+
+
 """Check if your throw are equal to your guess"""
 
 if __name__ == '__main__':
