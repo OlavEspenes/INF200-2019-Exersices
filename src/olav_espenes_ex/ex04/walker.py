b424557d9bd9b227ebf37929773a58415ef0d613
Olav Vikøren Espenes <olaves@nmbu.no>
2019-10-24 14:00:05 +0200

    Making rest of methods and output commando.

--- src/olav_espenes_ex/ex04/walker.py
+++ src/olav_espenes_ex/ex04/walker.py
@@ -19,4 +19,31 @@ class Walker:
     def is_at_home(self):
         return True if self.position == self. home else False
 
+    def get_position(self):
+        return self.position
+
+    def get_steps(self):
+        return self.steps
+
+    def walk_me_home_return_steps(self):
+        while not self.is_at_home():
+            self.move()
+        return self.steps
+
+
+if __name__=='__main__':
+    print(f'Distance:   1 -> Path lengths: '
+        f'{[Walker(0, 1).walk_me_home_return_steps() for _ in range(5)]}')
+    print(f'Distance:   2 -> Path lengths: '
+        f'{[Walker(0, 2).walk_me_home_return_steps() for _ in range(5)]}')
+    print(f'Distance:   5 -> Path lengths: '
+        f'{[Walker(0, 5).walk_me_home_return_steps() for _ in range(5)]}')
+    print(f'Distance:  10 -> Path lengths: '
+        f'{[Walker(0, 10).walk_me_home_return_steps() for _ in range(5)]}')
+    print(f'Distance:  20 -> Path lengths: '
+        f'{[Walker(0, 20).walk_me_home_return_steps() for _ in range(5)]}')
+    print(f'Distance:  50 -> Path lengths: '
+        f'{[Walker(0, 50).walk_me_home_return_steps() for _ in range(5)]}')
+    print(f'Distance: 100 -> Path lengths: '
+        f'{[Walker(0, 100).walk_me_home_return_steps() for _ in range(5)]}')
 
