a1d15694e9151ac695d1a6466cc39cee5a07a569
Olav Vikøren Espenes <olaves@nmbu.no>
2019-10-24 13:57:32 +0200

    Making def move and def is_at_home.

--- src/olav_espenes_ex/ex04/walker.py
+++ src/olav_espenes_ex/ex04/walker.py
@@ -1,1 +1,22 @@
-pass
+# -*- coding: utf-8 -*-
+
+__author__ = 'Olav Espenes'
+__email__ = 'olaves@nmbu.no'
+
+import random
+
+class Walker:
+    def __init__(self, start, home):
+        self.position = start
+        self.home = home
+        self.steps = 0
+
+    def move(self):
+        self.steps += 1
+        throw = random.uniform(0, 2)
+        self.position += 1 if throw >= 1 else -1
+
+    def is_at_home(self):
+        return True if self.position == self. home else False
+
+
