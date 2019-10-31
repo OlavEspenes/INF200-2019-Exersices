9efaa3e7effc2ce6c653190b2b7ac1a48febc7f2
Olav Vikøren Espenes <olaves@nmbu.no>
2019-10-31 16:44:46 +0100

    Make simulation class and init.

--- src/olav_espenes_ex/ex05/walker_sim.py
+++ src/olav_espenes_ex/ex05/walker_sim.py
@@ -2,3 +2,35 @@
 
 __author__ = 'Olav Espenes'
 __email__ = 'olaves@nmbu.no'
+
+import random
+
+class Walker:
+    def __init__(self, start, home):
+        self.position = start
+        self.home = home
+        self.steps = 0
+
+    def move(self):
+        self.steps += 1
+        throw = random.uniform(0, 2)
+        self.position += 1 if throw >= 1 else -1
+
+    def is_at_home(self):
+        return True if self.position == self. home else False
+
+    def get_position(self):
+        return self.position
+
+    def get_steps(self):
+        return self.steps
+
+class Simulation:
+    def __init__(self, start, home, seed):
+        self.start = start
+        self.home = home
+        random.seed(seed)
+
+    def single_walk(self):
+        a_walker = Walker(self.start, self.home)
+        
