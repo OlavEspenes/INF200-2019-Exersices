0403b1a29c37b11ea7ec64eb2e1d2712bc814c55
Olav Vikøren Espenes <olaves@nmbu.no>
2019-10-31 17:25:27 +0100

    Make print.

--- src/olav_espenes_ex/ex05/walker_sim.py
+++ src/olav_espenes_ex/ex05/walker_sim.py
@@ -33,11 +33,16 @@ class Simulation:
 
     def single_walk(self):
         a_walker = Walker(self.start, self.home)
-        while not a_walker.is_at_home()
+        while not a_walker.is_at_home():
             a_walker.move()
         return a_walker.get_steps()
 
     def run_simulation(self, num_walks):
         return[self.single_walk() for _ in range(num_walks)]
-    
-        
+
+if __name__=='__main__':
+    seed = [12345, 12345, 54321]
+    for one_seed in range(1,3):
+        print(Simulation(0,10,seed(one_seed)).run_simualtion(20))
+    for one_seed in range(1,3):
+        print(Simulation(10,0,seed(one_seed)).run_simulation(20))
