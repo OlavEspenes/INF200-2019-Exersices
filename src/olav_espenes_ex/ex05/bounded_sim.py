7caf986efdccb5f936c4cde735fc537723d2ebf6
Olav Vikøren Espenes <olaves@nmbu.no>
2019-11-03 16:13:10 +0100

    Doing B. Making C.

--- src/olav_espenes_ex/ex05/bounded_sim.py
+++ src/olav_espenes_ex/ex05/bounded_sim.py
@@ -3,44 +3,50 @@
 __author__ = 'Olav Espenes'
 __email__ = 'olaves@nmbu.no'
 
+from walker_sim import Walker
+from walker_sim import Simulation
+
 
 class BoundedWalker(Walker):
 
-    def __init__(self, start, home, left_limit, right_limit)
-        if not (left_limit <= start <= right_limit)
+    def __init__(self, start, home, left_limit, right_limit):
+        super().__init__(start, home)
+        self.left_limit = left_limit
+        self.right_limit = right_limit
+
+        if not (left_limit <= start <= right_limit):
             raise ValueError('The start position is not inside the input'
                              ' limit interval')
-        if not (left_limit <= home <= right_limit)
+        if not (left_limit <= home <= right_limit):
             raise ValueError('The home position is not inside the input'
                              ' interval limit')
-        super_class().__init__(start, home)
-        self.left = left_limit
-        self.right = right_limit
 
     def move(self):
 
-        super_class().move()
-        if self.position < self.left:
-            self. position = self.left
-        elif self. position > self.right:
-            self.position = self.right
-
+        super().move()
+        if self.position < self.left_limit:
+            self.position = self.left_limit
+        elif self.position > self.right_limit:
+            self.position = self.right_limit
 
 class BoundedSimulation(Simulation):
 
     def __init__(self, start, home, seed, left_limit, right_limit):
-        super_class().__init__(start, home, seed)
-        self.left = left_limit
-        self.right = right_limit
+        super().__init__(start, home, seed)
+        self.left_limit = left_limit
+        self.right_limit = right_limit
 
     def single_walk(self):
 
-        one_walker = BoundedWalker(self.start, self.home, self.left,
-                                   self.right)
+        one_walker = BoundedWalker(self.start, self.home, self.left_limit,
+                                   self.right_limit)
         steps = 0
-        while not walker.is_at_home():
-            walker.move()
+        while not one_walker.is_at_home():
+            one_walker.move()
             steps += 1
         return steps
 
-    
+if __name__ == '__main__':
+    for l_limit in [0, -10, -100, -1000, -10000]:
+        a_walk = BoundedSimulation(0, 20, 12345, l_limit, 20).run_simulation(20)
+        print('Left boundary: {}: {}'.format(l_limit, a_walk))
